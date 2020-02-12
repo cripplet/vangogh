d5d3d0b17498d2428629ef38252f16d15d251f24
Minke Zhang <mzhang@blogzhang.com>
2020-02-12 06:50:02 +0000

    Add no-op for core render function

---
+++ lib/core/render.go
@@ -0,0 +1,17 @@
+package vangogh_render
+
+import (
+    "github.com/cripplet/vangogh/api"
+    "github.com/cripplet/vangogh/proto"
+)
+
+type CoreRenderer struct {
+}
+
+func (r CoreRenderer) Generate(vangogh_proto.Blog) ([]vangogh_api.RenderOutput, error) {
+    return []vangogh_api.RenderOutput{}, nil
+}
+
+func (r CoreRenderer) Render([]vangogh_api.RenderOutput) error {
+    return nil
+}
