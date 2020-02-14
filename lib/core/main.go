3cc764d5b77de5d719a6c215151d9eda4490924a
Minke Zhang <mzhang@blogzhang.com>
2020-02-14 09:01:46 +0000

    Add enable simple render engine to output example textpb

--- lib/core/main.go
+++ lib/core/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "bytes"
   "io"
   "io/ioutil"
   "net/http"
@@ -9,7 +10,38 @@ import (
   "github.com/golang/protobuf/proto"
 )
 
+var directory map[string][]byte = map[string][]byte{}
+
 func main() {
+  var data []byte
+  var err error
+
+  var directory_readers map[string]io.Reader
+
+  data, err = ioutil.ReadFile("lib/api/proto/testdata/example.textpb")
+  if err != nil {
+    panic(err)
+    return
+  }
+
+  pb := vpb.Blog{}
+  if err = proto.UnmarshalText(string(data), &pb); err != nil {
+    panic(err)
+    return
+  }
+
+  directory_readers, err = vangogh_core_render.VangoghGenerate(pb)
+  if err != nil {
+    panic(err)
+    return
+  }
+
+  for u, r := range directory_readers {
+    b := bytes.Buffer{}
+    b.ReadFrom(r)
+    directory[u] = b.Bytes()
+  }
+
   http.HandleFunc("/", handler)
   http.ListenAndServe("0.0.0.0:8000", nil)
 }
@@ -17,31 +49,14 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html")
 
-  data, err := ioutil.ReadFile("lib/api/proto/testdata/example.textpb")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  pb := vpb.Blog{}
-  err = proto.UnmarshalText(string(data), &pb)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-  directory, err := vangogh_core_render.VangoghGenerate(pb)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  reader, is_found := directory[r.URL.Path]
+  b, is_found := directory[r.URL.Path]
   // TODO(minkezhang): Make this configurable as a page.
   if !is_found {
     http.Error(w, "404 NOT FOUND", http.StatusNotFound)
     return
   }
 
-  _, err = io.Copy(w, reader)
+  _, err := w.Write(b)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
--- lib/core/render.go
+++ lib/core/render.go
@@ -1,18 +1,37 @@
 package vangogh_core_render
 
 import (
+  "fmt"
+  "time"
+  "net/url"
   "html/template"
   "io"
   "path/filepath"
+  "regexp"
   "strings"
   vpb "github.com/cripplet/vangogh/api/proto"
+  "github.com/golang/protobuf/ptypes"
+  "github.com/golang/protobuf/ptypes/any"
+  "github.com/golang/protobuf/proto"
 )
+
+type ViewPostData struct {
+  Blog vpb.Blog
+  // Page-specific data to be used in generating the content template.
+  Content vpb.Post
+}
+
+func deserializeAnyProto(pb *any.Any) proto.Message {
+  var p ptypes.DynamicAny
+  ptypes.UnmarshalAny(pb, &p)
+  return p.Message
+}
 
 func VangoghGenerate(pb vpb.Blog) (map[string]io.Reader, error) {
   directory := map[string]io.Reader{}
 
   for _, p := range pb.Posts {
-    path, r, err := generatePost(*p)
+    path, r, err := generatePost(ViewPostData{Blog: pb, Content: *p})
     if err != nil {
       return nil, err
     }
@@ -22,7 +41,7 @@ func VangoghGenerate(pb vpb.Blog) (map[string]io.Reader, error) {
   return directory, nil
 }
 
-func generatePost(pb vpb.Post) (string, io.Reader, error) {
+func generatePost(v ViewPostData) (string, io.Reader, error) {
   f, err := getComponentFiles()
   if err != nil {
     return "", nil, err
@@ -33,11 +52,24 @@ func generatePost(pb vpb.Post) (string, io.Reader, error) {
   if err != nil {
     return "", nil, err
   }
-  err = t.Execute(&b, pb)
+  t.Funcs(
+    template.FuncMap{
+      "deserialize": deserializeAnyProto,
+    },
+  )
+
+  err = t.Execute(&b, v)
   if err != nil {
     return "", nil, err
   }
-  return "/", strings.NewReader(b.String()), nil
+  pt := time.Unix(v.Content.Metadata.PublishTimestamp.Seconds, int64(v.Content.Metadata.PublishTimestamp.Nanos))
+  r, err := regexp.Compile("[^a-zA-Z0-9\\-]+")
+  if err != nil {
+    return "", nil, err
+  }
+  path := "/posts/" + pt.Format("2006/01/02") + "/" + url.QueryEscape(r.ReplaceAllString(strings.ReplaceAll(strings.ToLower(v.Content.Metadata.Title), " ", "-"), ""))
+  fmt.Println(path)
+  return path, strings.NewReader(b.String()), nil
 }
 
 func getComponentFiles() ([]string, error) {
