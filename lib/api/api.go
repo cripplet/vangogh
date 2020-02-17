9f29b9fc19f61ac2fe1f1a7b6bd7465596c2732f
Minke Zhang <mzhang@blogzhang.com>
2020-02-17 22:31:52 +0000

    Add new RoutingTableRow type that will be implemented by rendering functions

--- lib/api/api.go
+++ lib/api/api.go
@@ -4,11 +4,32 @@
 package vangogh_api
 
 import (
+    "errors"
+    "fmt"
     "io"
+
     "github.com/cripplet/vangogh/api/proto"
 )
 
-type RoutingTable = map[string]io.Reader
+type RoutingTable map[string]io.Reader
+type RoutingTableRow struct {
+  Path string
+  Reader io.Reader
+}
+
+func (rt RoutingTable) AddRoutes(rs []RoutingTableRow) error {
+  for _, r := range rs {
+    if _, ok := rt[r.Path]; ok {
+      return errors.New(fmt.Sprintf("Duplicate route found: %s", r))
+    }
+  }
+
+  for _, r := range rs {
+    rt[r.Path] = r.Reader
+  }
+
+  return nil
+}
 
 // Interface VangoghRenderer is tasked with transforming the contents
 // encapsulated in the proto into a context-dependent consumable form.
